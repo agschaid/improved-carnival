4d201b3385e461b303518dea61b36f45b51f0ca1
Axel Gschaider <axel.gschaider@posteo.de>
2020-06-29 06:52:07 +0200

    colorized dark and light

--- user-configuration.nix
+++ user-configuration.nix
@@ -34,6 +34,17 @@ let
     #!${pkgs.stdenv.shell}
     systemctl $1 openvpn-private.service
   '';
+
+  dark_theme = pkgs.writeScriptBin "dark_theme" ''
+    #!${pkgs.stdenv.shell}
+    kitty @set-colors --all --configured ~/.config/kitty/solarized-dark.conf
+  '';
+
+  light_theme = pkgs.writeScriptBin "light_theme" ''
+    #!${pkgs.stdenv.shell}
+    kitty @set-colors --all --configured ~/.config/kitty/solarized-light.conf
+  '';
+
 in
 {
   imports = [
@@ -73,41 +84,79 @@ in
       mongo_connect
       vpn_office
       vpn_private
+      light_theme
+      dark_theme
       ];
 
     home.file.".daSepp".text = ''
       alpenrap
     '';
 
+    home.file.".config/kitty/solarized-dark.conf".text = ''
+        background              #002b36
+        foreground              #839496
+        cursor                  #93a1a1
+        selection_background    #81908f
+        selection_foreground    #002831 
+        color0                  #073642
+        color1                  #dc322f
+        color10                 #586e75
+        color11                 #657b83
+        color12                 #839496
+        color13                 #6c71c4
+        color14                 #93a1a1
+        color15                 #fdf6e3
+        color2                  #859900
+        color3                  #b58900
+        color4                  #268bd2
+        color5                  #d33682
+        color6                  #2aa198
+        color7                  #eee8d5
+        color8                  #002b36
+        color9                  #cb4b16
+    '';
+
+    home.file.".config/kitty/solarized-light.conf".text = ''
+    background              #fdf6e3
+foreground              #657b83
+cursor                  #586e75
+
+selection_background    #475b62
+selection_foreground    #eae3cb
+
+color0                #073642
+color8                #002b36
+
+color1                #dc322f
+color9                #cb4b16
+
+color2                #859900
+color10               #586e75
+
+color3                #b58900
+color11               #657b83
+
+color4                #268bd2
+color12               #839496
+
+color5                #d33682
+color13               #6c71c4
+
+color6                #2aa198
+color14               #93a1a1
+
+color7                #eee8d5
+color15               #fdf6e3
+    '';
+
     home.file.".config/kitty/kitty.conf".text = ''
-font_family Victor Mono
-
-allow_remote_control yes
-
-background #002b36
-color0 #073642
-color1 #dc322f
-color10 #586e75
-color11 #657b83
-color12 #839496
-color13 #6c71c4
-color14 #93a1a1
-color15 #fdf6e3
-color2 #859900
-color3 #b58900
-color4 #268bd2
-color5 #d33682
-color6 #2aa198
-color7 #eee8d5
-color8 #002b36
-color9 #cb4b16
-cursor #93a1a1
-cursor_shape beam
-enable_audio_bell no
-font_size 12
-foreground #839496
-selection_background #81908f
-selection_foreground #002831 
+        font_family Victor Mono
+        
+        allow_remote_control yes
+        
+        cursor_shape beam
+        enable_audio_bell no
+        font_size 12
     '';
 
     programs.git = {
@@ -201,6 +250,7 @@ selection_foreground #002831
         
         " show dot files by default
         let NERDTreeShowHidden=1
+        :hi Directory ctermfg=4  " fix directory coloring of NerdTree
 
         " GOYO CONFIG
         " by default start in programming linebreak width
