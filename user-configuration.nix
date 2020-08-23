9b1bdc1eb075cd65648c2a27858df51074eda7bd
Axel Gschaider <axel.gschaider@posteo.de>
2020-08-24 00:19:19 +0200

    some mappings

--- user-configuration.nix
+++ user-configuration.nix
@@ -290,12 +290,17 @@ in
         let g:ale_java_eclipselsp_path = '/home/agl/github/eclipse.jdt.ls'
         let g:ale_completion_enabled = 1
         " let g:ale_sign_column_always = 1
+
         let g:ale_sign_error = '>>'
         let g:ale_sign_warning = '--'
         highlight ALEErrorSign ctermbg=125 ctermfg=235 " Solarized Magenta
         highlight ALEError ctermbg=125 ctermfg=235 " Solarized Magenta
         highlight ALEWarningSign ctermbg=136 ctermfg=235 " Solarized Yellow
         highlight ALEWarning ctermbg=136 ctermfg=235 " Solarized Yellow
+        
+        :map <silent> <C-A-j> <Plug>(ale_next_wrap)
+        :map <silent> <C-A-k> <Plug>(ale_previous_wrap)
+
         let g:ale_linters = {
             \ 'java':['eclipselsp'],
         \}
