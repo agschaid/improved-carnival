c4dcda9d1aaa26c62683bf5ef7873bb078db6c90
Axel Gschaider <axel.gschaider@posteo.de>
2021-07-12 00:20:59 +0200

    add mullvad vpn client

--- configuration.nix
+++ configuration.nix
@@ -285,6 +285,7 @@
       hydrogen
       musescore
 
+      mullvad-vpn
     ];
 
   fonts.fonts = with pkgs; [
@@ -315,6 +316,8 @@
 
   # Enable the OpenSSH daemon.
   # services.openssh.enable = true;
+
+  services.mullvad-vpn.enable = true;
 
   # Open ports in the firewall.
   networking.firewall.allowedTCPPorts = [ 
