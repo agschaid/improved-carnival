d0c524037c9abd12d5438cb081a277d5916a4c10
Axel Gschaider <axel.gschaider@posteo.de>
2023-01-19 08:05:06 +0100

    add abcde config

--- user-configuration.nix
+++ user-configuration.nix
@@ -487,6 +487,7 @@ in
     home.file.".config/fish/completions/pass.fish".source = ./dotfiles/fish/completions/pass.fish;
     home.file.".wezterm.lua".source = ./dotfiles/wezterm/wezterm.lua;
     home.file.".config/topydo/config".source = ./dotfiles/topydo/config;
+    home.file.".abcde.conf".source = ./dotfiles/abcde/abcde.conf;
     
   };
 }
